24aa4fdcf7f3ab9dee29d9282ae4a19341666627
betassamita <betassamita@gmail.com>
2026-05-26 01:12:54 -0500

    Problema 2 Fase 5

--- Fase_5_1.py
+++ Fase_5_1.py
@@ -10,3 +10,34 @@ Menu = [
     ["Ramen", "Comida Japonesa", 32000],
     ["Lasagna", "Comida Italiana", 25000]
 	]
+# Se definen la categoría objetivo y el umbral de descuento para la promoción
+Categoria_Objetivo = "Comida Mexicana"
+umbral_descuento = 19000
+def calcular_precio_final(categoria, precio_base):
+    descuento = 0.15
+
+    # Aplicamos la promoción según las condiciones
+    if categoria == Categoria_Objetivo and precio_base > umbral_descuento:
+        precio_final = precio_base - (precio_base * descuento)
+    else:
+        precio_final = precio_base
+
+    return precio_final
+
+# Mostrar el menú con promociones
+print("Menu con promociones aplicadas\n")
+	
+for producto in Menu:
+	    nombre = producto[0]
+categoria = producto[1]
+precio_base = producto[2]
+	    
+	    # Se calcula el precio final
+precio_final = calcular_precio_final(categoria, precio_base)
+	    
+	    # Se muestra la información final
+print(f"Producto:        {nombre}")
+print(f"Categoría:       {categoria}")
+print(f"Precio base:     $ {precio_base}")
+print(f"Precio final:    $ {precio_final}")
+print("=" * 40)
